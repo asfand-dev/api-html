396885918107f1443cb0898963fdb6b18e7cb9f8
Asfandiyar Khan <asfandiyar.khan@cloud.ionos.com>
2020-10-05 08:45:29 +0200

    Updated the docs build for github page

--- docs/precache-manifest.9c5c5976f1eed93e18ea76bffee1f1ca.js
+++ docs/precache-manifest.9c5c5976f1eed93e18ea76bffee1f1ca.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ebb6fdad01655c3450f18bf717dcac86",
+    "revision": "464ca1a3fdac608776cbd78d69bd85d7",
     "url": "/swagger-theme-online/index.html"
   },
   {
-    "revision": "3499dc6a8db6ec2a07a8",
+    "revision": "ed0c0100d19b28182c0e",
     "url": "/swagger-theme-online/static/css/2.86a1bf4f.chunk.css"
   },
   {
-    "revision": "df3e704afacbc7a52e12",
+    "revision": "d976d9150ece57bbf5fd",
     "url": "/swagger-theme-online/static/css/main.27c09b05.chunk.css"
   },
   {
-    "revision": "3499dc6a8db6ec2a07a8",
-    "url": "/swagger-theme-online/static/js/2.fafa3a63.chunk.js"
+    "revision": "ed0c0100d19b28182c0e",
+    "url": "/swagger-theme-online/static/js/2.4dd333f1.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/swagger-theme-online/static/js/2.fafa3a63.chunk.js.LICENSE.txt"
+    "url": "/swagger-theme-online/static/js/2.4dd333f1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "df3e704afacbc7a52e12",
-    "url": "/swagger-theme-online/static/js/main.71744749.chunk.js"
+    "revision": "d976d9150ece57bbf5fd",
+    "url": "/swagger-theme-online/static/js/main.44276f9a.chunk.js"
   },
   {
     "revision": "348bb985d84555b91f01",
